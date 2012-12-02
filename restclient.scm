bc34e1d16c961c4737b0ecaf572e397fadd82e1e
Rich Loveland <loveland.richard@gmail.com>
2012-12-02 11:05:12 -0500

    Added copyright notice and a brief description.

--- restclient.scm
+++ restclient.scm
@@ -1,3 +1,28 @@
+#| -*-Scheme-*-
+
+Copyright (C) 2012 Rich Loveland <loveland.richard@gmail.com>
+
+This file is NOT part of MIT/GNU Scheme.
+
+MIT/GNU Scheme is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
+
+MIT/GNU Scheme is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MIT/GNU Scheme; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301,
+USA.
+
+|#
+
+;;;; REST client integration for Edwin.
+
 (define api-urls '((console-prod . "http://api.appnexus.com")
                    (console-sand . "http://sand.api.appnexus.com")))
 
